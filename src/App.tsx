a269f93247195b7df383b3720fc915220d13a24a
Nellyojay <nellyjohnson696@gmail.com>
2026-03-26 17:03:12 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-green-500'>Hottish</h1>
+        <p className='text-red-500'>Welcome to Hottish</p>
+
+        <a href="" className='text-2xl font-bold font-serif'>okudi</a>
+
+        <h2 className='text-red-500'>John</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
